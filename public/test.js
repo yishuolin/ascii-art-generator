1934bc5029ae2bb351c7841b8dc9e4bb385d9b47
yslin <s5907610@gmail.com>
2021-08-02 22:13:17 +0800

    feat: implement basic function

---
+++ public/test.js
@@ -0,0 +1,22 @@
+const string = 'testing string';
+const file = new File([string], 'foo.txt', {
+  type: 'text/plain',
+});
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute(
+    'href',
+    'data:text/plain;charset=utf-8,' + encodeURIComponent(text),
+  );
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+download('test.txt', string);
