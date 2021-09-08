6f85dbaa83ddc94d1b40609ef56491f35bb524e5
nico <nthunico@gmail.com>
2021-09-08 23:37:41 +0800

    style：button style changed

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,42 @@
 import React, { useState, useRef } from 'react';
 import Head from 'next/head';
-import { Button } from '@material-ui/core';
 import { CustomSlider } from '../components';
 import { getGrayscaleResult, getColorfulResult } from '../helpers';
 import styled from '@emotion/styled';
+
+const Button = styled.button`
+  background-color: #9faeda;
+  color: #ffffff;
+  font-size: 16px;
+  font-family: Verdana;
+  padding: 4px 6px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
+`;
+
+const Heading = styled.div`
+  padding-top: 40px;
+  padding-left: 5%;
+  padding-bottom: 30px;
+  color: white;
+  font-size: 50px;
+  font-family: Verdana;
+  background-color: #9faeda;
+`;
+
+const Steps = styled.span`
+  font-weight: bold;
+  color: #526ebc;
+`;
+
+const ContentText = styled.div`
+  margin-top: 50px;
+  margin-left: 5%;
+  color: #657ec3;
+  font-size: 15px;
+  font-family: Verdana;
+`;
 
 export default function Home() {
   const MIN_WIDTH = 50;
@@ -55,38 +88,6 @@ export default function Home() {
     setWidth(newValue);
   };
 
-  const Button = styled.button`
-    margin-left: 9.3%;
-    margin-top: 20px;
-    background-color: #9faeda;
-    color: #ffffff;
-    font-size: 16px;
-    font-family: Verdana;
-  `;
-
-  const Heading = styled.div`
-    padding-top: 40px;
-    padding-left: 5%;
-    padding-bottom: 30px;
-    color: white;
-    font-size: 50px;
-    font-family: Verdana;
-    background-color: #9faeda;
-  `;
-
-  const Steps = styled.span`
-    font-weight: bold;
-    color: #526ebc;
-  `;
-
-  const ContentText = styled.div`
-    margin-top: 50px;
-    margin-left: 5%;
-    color: #657ec3;
-    font-size: 15px;
-    font-family: Verdana;
-  `;
-
   return (
     <div>
       <Head>
@@ -113,11 +114,9 @@ export default function Home() {
           Upload a picture.
         </ContentText>
 
-        <div>
-          <Button>
-            <label htmlFor="contained-button-file">Upload</label>
-          </Button>
-        </div>
+        <Button style={{ marginLeft: '9.3%', marginTop: '20px' }}>
+          <label htmlFor="contained-button-file">Upload</label>
+        </Button>
 
         <div
           style={{
@@ -134,39 +133,36 @@ export default function Home() {
           <Steps>Step 2:</Steps> Character density.
         </ContentText>
 
-        <div>
-          <CustomSlider
-            style={{ width: '250px', marginTop: '20px', marginLeft: '9.3%' }}
-            defaultValue={width}
-            value={width}
-            onChange={handleUpdateWidth}
-            marks
-            min={MIN_WIDTH}
-            max={MAX_WIDTH}
-            valueLabelDisplay="auto"
-          />
-          {loading && (
-            <span
-              style={{
-                marginLeft: '30%',
-                marginTop: '20px',
-                color: '#657ec3',
-              }}>
-              Loading.....
-            </span>
-          )}
-        </div>
+        <CustomSlider
+          style={{ width: '250px', marginTop: '20px', marginLeft: '9.3%' }}
+          defaultValue={width}
+          value={width}
+          onChange={handleUpdateWidth}
+          marks
+          min={MIN_WIDTH}
+          max={MAX_WIDTH}
+          valueLabelDisplay="auto"
+        />
+        {loading && (
+          <span
+            style={{
+              marginLeft: '30%',
+              marginTop: '20px',
+              color: '#657ec3',
+            }}>
+            Loading.....
+          </span>
+        )}
 
         <ContentText>
           <Steps>Step 3:</Steps> Choose the color scheme generated.
         </ContentText>
 
-        <div>
+        <div style={{ marginLeft: '9.3%', marginTop: '20px' }}>
           <Button disabled={!file} onClick={handleGrayscaleImage}>
             Grayscale
           </Button>
-        </div>
-        <div>
+
           <Button disabled={!file} onClick={handleColorfulImage}>
             Colorful
           </Button>
@@ -176,17 +172,13 @@ export default function Home() {
           <Steps>Step 4:</Steps> Download.
         </ContentText>
 
-        <div>
-          <Button>
-            <a
-              href={`data:text/plain;charset=utf-8,${encodeURIComponent(
-                output,
-              )}`}
-              download={output.startsWith('<div') ? 'art.html' : 'art.txt'}>
-              Download
-            </a>
-          </Button>
-        </div>
+        <Button style={{ marginLeft: '9.3%', marginTop: '20px' }}>
+          <a
+            href={`data:text/plain;charset=utf-8,${encodeURIComponent(output)}`}
+            download={output.startsWith('<div') ? 'art.html' : 'art.txt'}>
+            Download
+          </a>
+        </Button>
 
         <div
           ref={result}
@@ -194,7 +186,7 @@ export default function Home() {
             fontFamily: 'monospace',
             fontSize,
             marginLeft: '29.5%',
-            marginTop: '-535px',
+            marginTop: '-500px',
           }}></div>
       </div>
     </div>
